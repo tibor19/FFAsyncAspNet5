ea90ffe8f883afa008ba44e5c011c8fbdd3e0b66
Tiberiu Covaci <tibi@covaci.se>
2017-01-30 12:40:42 +0000

    PageAsyncTask with Task

--- FFAspNet5/DisplayRecipe.aspx.cs
+++ FFAspNet5/DisplayRecipe.aspx.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -22,10 +23,10 @@ namespace FFAspNet5
             int recipeId = 0;
             if (Int32.TryParse(Request.QueryString["ID"], out recipeId) && recipeId != 0)
             {
-                lblMethod.Text = "RegisterAsyncTask";
-                GetDetails(recipeId);
-                GetIngredients(recipeId);
-                GetInstructions(recipeId);
+                lblMethod.Text = "PageAsyncTask with Task";
+                Page.RegisterAsyncTask(new PageAsyncTask(() => GetDetailsAsync(recipeId)));
+                Page.RegisterAsyncTask(new PageAsyncTask(() => GetIngredientsAsync(recipeId)));
+                Page.RegisterAsyncTask(new PageAsyncTask(() => GetInstructionsAsync(recipeId)));
             }
         }
 
@@ -36,7 +37,7 @@ namespace FFAspNet5
             base.OnPreRenderComplete(e);
         }
 
-        private void GetDetails(int recipeId)
+        private async Task GetDetailsAsync(int recipeId)
         {
             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
             conn.Open();
@@ -46,28 +47,20 @@ namespace FFAspNet5
             cmd.Parameters.AddWithValue("@RecipeID", recipeId);
 
             // for ASP.NET 2.0 applications use delegates instead of lamdba expressions
-            Page.RegisterAsyncTask(new PageAsyncTask(
-                (sender, eventArg, callback, state) => cmd.BeginExecuteReader(callback, state), // beginHandler
-                ar => { // endHandler
-
-                    using (SqlDataReader reader = cmd.EndExecuteReader(ar))
-                    {
-                        if (reader.Read())
-                        {
-                            RecipeName.Text = reader.GetString(0);
-                            RecipeImage.ImageUrl = @"Content/Images/food/" + reader.GetString(1) + ".jpg";
-                            RecipePrice.Text = reader.GetDecimal(2).ToString();
-                            RecipeTime.Text = reader.GetInt32(3).ToString();
-                        }
-                    }
-                    conn.Close();
-                },
-                null, // timeoutHandler
-                null // state
-                ));
+            using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+            {
+                if (reader.Read())
+                {
+                    RecipeName.Text = reader.GetString(0);
+                    RecipeImage.ImageUrl = @"Content/Images/food/" + reader.GetString(1) + ".jpg";
+                    RecipePrice.Text = reader.GetDecimal(2).ToString();
+                    RecipeTime.Text = reader.GetInt32(3).ToString();
+                }
+            }
+            conn.Close();
         }
 
-        private void GetIngredients(int recipeId)
+        private async Task GetIngredientsAsync(int recipeId)
         {
             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
             conn.Open();
@@ -76,22 +69,15 @@ namespace FFAspNet5
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@RecipeID", recipeId);
 
-            Page.RegisterAsyncTask(new PageAsyncTask(
-                (sender, eventArg, callback, state) => cmd.BeginExecuteReader(callback, state), // beginHandler
-                ar => { // endHandler
-                    using (SqlDataReader reader = cmd.EndExecuteReader(ar))
-                    {
-                        lstRecipeIngredients.DataSource = reader;
-                        lstRecipeIngredients.DataBind();
-                    }
-                    conn.Close();
-                },
-                null, // timeoutHandler
-                null // state
-                ));
+            using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+            {
+                lstRecipeIngredients.DataSource = reader;
+                lstRecipeIngredients.DataBind();
+            }
+            conn.Close();
         }
 
-        private void GetInstructions(int recipeId)
+        private async Task GetInstructionsAsync(int recipeId)
         {
             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
             conn.Open();
@@ -100,19 +86,12 @@ namespace FFAspNet5
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@RecipeID", recipeId);
 
-            Page.RegisterAsyncTask(new PageAsyncTask(
-                (sender, eventArg, callback, state) => cmd.BeginExecuteReader(callback, state), // beginHandler
-                ar => { // endHandler
-                    using (SqlDataReader reader = cmd.EndExecuteReader(ar))
-                    {
-                        lstRecipeInstructions.DataSource = reader;
-                        lstRecipeInstructions.DataBind();
-                    }
-                    conn.Close();
-                },
-                null, // timeoutHandler
-                null // state
-                ));
+            using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+            {
+                lstRecipeInstructions.DataSource = reader;
+                lstRecipeInstructions.DataBind();
+            }
+            conn.Close();
         }
     }
 }
